abfe3b874c0239aac07c86472d8aac1b130316b3
Arvid Edenheim <arved490@student.liu.se>
2019-09-14 22:15:38 +0200

    Updates

---
+++ precache-manifest.19a58c01a3198c3a43c526be56c1bb7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2a37532739d291153e6d08e98c03e8",
+    "url": "/disco/index.html"
+  },
+  {
+    "revision": "5ad0c7f64f494a07f859",
+    "url": "/disco/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "1c9d100fbc40eaa7e471",
+    "url": "/disco/static/js/2.96c7d7de.chunk.js"
+  },
+  {
+    "revision": "5ad0c7f64f494a07f859",
+    "url": "/disco/static/js/main.2fb4a57f.chunk.js"
+  },
+  {
+    "revision": "2f360448be6acbe76851",
+    "url": "/disco/static/js/runtime~main.7fe3f356.js"
+  }
+]);
